eea74c7e7b6690b16709d5f4d526b04c51d7b1ba
neefrankie <neefrankie@gmail.com>
2016-10-16 21:08:21 +0800

    change to manual scan to avoid github rate limit

--- scanner.js
+++ scanner.js
@@ -5,18 +5,38 @@ const request = require('request-promise-native');
 const co = require('co');
 const mkdirp = require('mkdirp');
 const str = require('string-to-stream');
+const minimist = require('minimist');
+const argv = minimist(process.argv.slice(2), {
+  string: ['input'],
+  boolean: 'all',
+  alias: {
+    i: 'input',
+    a: 'all'
+  }
+});
 
 const helper = require('./helper');
-const moduleNames = require('./module-list.json');
+const moduleList = require('./module-list.json');
 
 const manifests = [
 	'bower',
 	'origami'
 ];
 
+var moduleNames = null;
+
+if (argv.i && moduleList.indexOf(argv.i) !== -1) {
+	moduleNames = [argv.i];
+} else if (argv.a) {
+	moduleNames = moduleList;
+} else {
+	console.log(`You should provide a module name as listed in "module-list.json"`);
+	return;
+}
+
 moduleNames.forEach((moduleName) => {
 	co(function *() {
-		// const moduleName = 'ftc-share';
+		// get the url for bower.json, origami.json and tags
 		const targetUrls = buildUrls(moduleName);
 		const moduleData = {
 			moduleName: moduleName,
@@ -24,25 +44,43 @@ moduleNames.forEach((moduleName) => {
 			repoHomeUrl: repoHomeUrl(moduleName),
 			isStable: true,
 		}
-		console.log(targetUrls);
 
-		const options = targetUrls.map(requestOptions);
+		// construct request options for `request`
+		const options = targetUrls.map(url => {
+			return buildOpts(url);
+		});
 
-		var requestData = yield Promise.all(options.map(request));
-		var [contentBower, contentOrigami, refTags] = requestData.map(JSON.parse);
+		// Execute `request`
+		var response = yield Promise.all(options.map(option => {
+			console.log(`Requesting ${option.url}`);
+			return request(option);
+		}));
+		// Get data from 3 urls
+		var [contentBower, contentOrigami, refTags] = response.map(JSON.parse);
+
 		const bower = decodeContent(contentBower);
 		const origami = decodeContent(contentOrigami);
 
+		// .hasCss, .hasJs, .dependencies
 		const extracted = extractBower(bower);
 
 		const versions = getVersions(refTags);
-
 
 		const latest = getLatestStable(versions);
 		moduleData.tagName = latest.tagName;
 
 		console.log('requesting: ', latest.tagUrl);
-		const latestVersionData = yield request(requestOptions(latest.tagUrl));
+		// Get data from latest version's url, to get tagged time.
+/* Format:
+{
+  "tagger": {
+    "name": "",
+    "email": "",
+    "date": "2015-10-30T04:09:34Z"
+  }
+}
+*/
+		const latestVersionData = yield request(buildOpts(latest.tagUrl));
 
 		const datetimeCreated = JSON.parse(latestVersionData).tagger.date;
 		moduleData.datetimeCreated = datetimeCreated;
@@ -52,7 +90,6 @@ moduleNames.forEach((moduleName) => {
 		});
 
 		Object.assign(moduleData, extracted, origami);
-		console.log(moduleData);
 
 		str(JSON.stringify(moduleData, null, 4))
 			.pipe(fs.createWriteStream(`data/${moduleName}.json`));
@@ -65,6 +102,16 @@ moduleNames.forEach((moduleName) => {
 
 });
 
+/*
+ * @param {String} module - A module name
+ * @return {Array}
+ * @example
+ * [
+	https://api.github.com/repos/FTChinese/ftc-share/contents/bower.json,
+	https://api.github.com/repos/FTChinese/ftc-share/contents/origami.json,
+	https://api.github.com/repos/FTChinese/ftc-share/git/refs/tags
+ * ]
+ */
 function buildUrls(module) {
 	const targetUrls = manifests.map(function(manifest) {
 		return `https://api.github.com/repos/FTChinese/${module}/contents/${manifest}.json`;
@@ -73,12 +120,20 @@ function buildUrls(module) {
 	targetUrls.push(`https://api.github.com/repos/FTChinese/${module}/git/refs/tags`);
 	return targetUrls;
 }
-
+/**
+ * @param {String} module - Module name
+ * @return {String} - Module's repository url
+ * @example https://github.com/FTChinese/ftc-share
+ */
 function repoHomeUrl(module) {
 	return `https://github.com/FTChinese/${module}`;
 }
 
-function requestOptions(url) {
+/**
+ * Github api requires a request header.
+ * @param {String} - The url to request
+ */
+function buildOpts(url) {
 	return {
 		url: url,
 		headers: {
@@ -87,6 +142,18 @@ function requestOptions(url) {
 	}
 }
 
+/*
+ * Github returned content are encoded in base64.
+ */
+function decodeContent(data) {
+	const buf = Buffer.from(data.content, data.encoding);
+	return JSON.parse(buf.toString());
+}
+
+/*
+ * @param {Object} bower - bower.json content
+ * @return {Object} - object.hasCss, object.hasJs, object.dependencies.
+ */
 function extractBower(bower) {
 	var obj = {};
 
@@ -99,17 +166,18 @@ function extractBower(bower) {
 	return obj;
 }
 
-function decodeContent(data) {
-	const buf = Buffer.from(data.content, data.encoding);
-	return JSON.parse(buf.toString());
-}
-
-function stripTagName(ref) {
-	var tmp = ref.split('/');
-  tmp = tmp[tmp.length - 1].replace('v', '');
-	return tmp;
-}
-
+/*
+ * @param {Array} refTags - Returned from `refs/tags`. Each item is an object.
+ * @example
+   {
+    "ref": "refs/tags/v0.0.1",
+    "url": "https://api.github.com/repos/FTChinese/ftc-share/git/refs/tags/v0.0.1",
+    "object": {
+      "sha": "b03db37bff21cf88a1e4a7a1b85db9497be4c437",
+      "type": "tag",
+      "url": "https://api.github.com/repos/FTChinese/ftc-share/git/tags/b03db37bff21cf88a1e4a7a1b85db9497be4c437"
+    }
+ */
 function getVersions(refTags) {
 	return refTags.map((tag) => {
 		return {
@@ -121,6 +189,18 @@ function getVersions(refTags) {
 	}).reverse();
 }
 
+function stripTagName(ref) {
+	var tmp = ref.split('/');
+  tmp = tmp[tmp.length - 1].replace('v', '');
+	return tmp;
+}
+
+/**
+ * @param {Array} versions - simplified `refTags`
+ * @return {Object}
+ * @return {String} object.tagName - like 0.0.1
+ * @return {String} object.tagUrl - This tag's api url. Contains tagged time.
+ */
 function getLatestStable(versions) {
 	var tagName = '';
 	var tagUrl = '';
